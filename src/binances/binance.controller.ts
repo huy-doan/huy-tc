c35a411d7a3e96d71eb180533e62234171c4cee8
Huy Doan <huydq@vietnamlab.vn>
2025-05-06 21:45:37 +0700

    imp

--- src/binances/binance.controller.ts
+++ src/binances/binance.controller.ts
@@ -1,7 +1,6 @@
 // src/binances/binance.controller.ts
 import { Controller, Get, Query } from '@nestjs/common';
 import { BinanceService } from './binance.service';
-import { OldBinanceService } from './old-binance.service';
 import { AnalyzeResult } from '~interfaces/common.interface';
 import { CandleChartInterval_LT } from 'binance-api-node';
 
@@ -9,70 +8,13 @@ import { CandleChartInterval_LT } from 'binance-api-node';
 export class BinanceController {
     constructor(
         private readonly binanceService: BinanceService,
-        private readonly oldBinanceService: OldBinanceService
     ) {}
 
-    @Get('/old-cypher')
+    @Get('/all')
     async oldCypher(
         @Query('interval') interval: CandleChartInterval_LT, 
         @Query('limit') limit: number
     ): Promise<AnalyzeResult[]> {
-        return this.oldBinanceService.harmonicCypherPattern(interval, limit);
-    }
-
-    @Get('/cypher')
-    async cypher(
-        @Query('interval') interval: CandleChartInterval_LT, 
-        @Query('limit') limit: number
-    ): Promise<AnalyzeResult[]> {
         return this.binanceService.harmonicCypherPattern(interval, limit);
-    }
-    
-    @Get('/bat')
-    async bat(
-        @Query('interval') interval: CandleChartInterval_LT, 
-        @Query('limit') limit: number
-    ): Promise<AnalyzeResult[]> {
-        return this.binanceService.harmonicBatPattern(interval, limit);
-    }
-    
-    @Get('/gartley')
-    async gartley(
-        @Query('interval') interval: CandleChartInterval_LT, 
-        @Query('limit') limit: number
-    ): Promise<AnalyzeResult[]> {
-        return this.binanceService.harmonicGartleyPattern(interval, limit);
-    }
-    
-    @Get('/butterfly')
-    async butterfly(
-        @Query('interval') interval: CandleChartInterval_LT, 
-        @Query('limit') limit: number
-    ): Promise<AnalyzeResult[]> {
-        return this.binanceService.harmonicButterflyPattern(interval, limit);
-    }
-    
-    @Get('/crab')
-    async crab(
-        @Query('interval') interval: CandleChartInterval_LT, 
-        @Query('limit') limit: number
-    ): Promise<AnalyzeResult[]> {
-        return this.binanceService.harmonicCrabPattern(interval, limit);
-    }
-    
-    @Get('/shark')
-    async shark(
-        @Query('interval') interval: CandleChartInterval_LT, 
-        @Query('limit') limit: number
-    ): Promise<AnalyzeResult[]> {
-        return this.binanceService.harmonicSharkPattern(interval, limit);
-    }
-    
-    @Get('/all')
-    async allPatterns(
-        @Query('interval') interval: CandleChartInterval_LT, 
-        @Query('limit') limit: number
-    ): Promise<AnalyzeResult[]> {
-        return this.binanceService.analyzeHarmonicPatterns(interval, limit);
     }
 }
